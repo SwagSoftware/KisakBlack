4987f20e1e1a5bf4308a4cdfc300080926e4a7d8
Simasce <siamsce@gmail.com>
2026-05-13 22:24:14 +0300

    Changed DB stream assertions to bcassert, returned definitions

--- src/gfx_d3d/r_stream.cpp
+++ src/gfx_d3d/r_stream.cpp
@@ -15,7 +15,6 @@
 #include "r_image.h"
 #include "rb_resource.h"
 #include <database/db_file_load.h>
-#include <database/db_registry.h>
 #include <universal/com_workercmds.h>
 #include <cgame/cg_compass.h>
 #include <win32/win_shared.h>
@@ -25,6 +24,8 @@
 #include <DynEntity/DynEntity_load_obj.h>
 #include <DynEntity/DynEntity_client.h>
 #include "r_rendercmds.h"
+
+#define TOTAL_IMAGE_PARTS 4608 // Same as POOLSIZE_IMAGE
 
 volatile unsigned int r_stream_sortLimit = 1;
 jqModule r_stream_sortModule =
@@ -1005,10 +1006,7 @@ void __cdecl R_Stream_AddImagePartImportance(int imagePartIndex, float importanc
 {
     float v2; // [esp+0h] [ebp-8h]
 
-    vassert(imagePartIndex < POOLSIZE_IMAGE,
-        "imagePartIndex doesn't index TOTAL_IMAGE_PARTS\n\t%i not in [0, %i)",
-        imagePartIndex,
-        POOLSIZE_IMAGE);
+    bcassert(imagePartIndex, TOTAL_IMAGE_PARTS);
 
     if ((float)(importance - *(float *)&streamFrontendGlob.imageImportanceBits[imagePartIndex - 4064]) < 0.0)
         v2 = *(float *)&streamFrontendGlob.imageImportanceBits[imagePartIndex - 4064];
